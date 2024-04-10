87631ae7e6eb3a6058660e6eb4ff22959bdf455e
Moa-IcI <151546658+Moa-IcI@users.noreply.github.com>
2024-04-09 18:22:43 -0700

    Update addapps.py
    input name of app
    
    this will append code to your file

--- addapps.py
+++ addapps.py
@@ -1,1 +1,7 @@
-import os
+print("Input the exact name of the app this will append it to the open-apps.txt file with correct code")
+appendtofile = input()
+applcode = 'set question to display dialog "Choose your app." buttons {"' + appendtofile + '", "Quit", "Next page"}\nset answer to button returned of question\nif answer = "Finder" then\n	tell application answer to activate\nend if\nif answer = "Quit" then\n	tell "Open-Apps" to quit\nend if'
+
+f = open("open-apps.txt", "a")
+f.write(applcode)
+f.close()
