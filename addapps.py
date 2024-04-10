43d4cc737ae2b870a44516d8cfe2835ac260a5b0
Moa-IcI <151546658+Moa-IcI@users.noreply.github.com>
2024-04-09 18:26:20 -0700

    Update addapps.py
    renames the file of code to .scpt

--- addapps.py
+++ addapps.py
@@ -1,3 +1,4 @@
+import os
 print("Input the exact name of the app this will append it to the open-apps.txt file with correct code")
 appendtofile = input()
 applcode = 'set question to display dialog "Choose your app." buttons {"' + appendtofile + '", "Quit", "Next page"}\nset answer to button returned of question\nif answer = "Finder" then\n	tell application answer to activate\nend if\nif answer = "Quit" then\n	tell "Open-Apps" to quit\nend if'
@@ -5,3 +6,4 @@ applcode = 'set question to display dialog "Choose your app." buttons {"' + appe
 f = open("open-apps.txt", "a")
 f.write(applcode)
 f.close()
+os.rename('open-apps.txt', 'open-apps.scpt')
